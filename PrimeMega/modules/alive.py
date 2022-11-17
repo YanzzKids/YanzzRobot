393a5af7a1c54f895bc2657fc4fea6ca37f81723
BionXP <112658830+BionXP@users.noreply.github.com>
2022-11-18 03:58:48 +0800

    Update alive.py

--- PrimeMega/modules/alive.py
+++ PrimeMega/modules/alive.py
@@ -7,18 +7,18 @@ from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
 from PrimeMega.events import register
 from PrimeMega import telethn as tbot
-from PrimeMega import dispatcher, OWNER_USERNAME as uname, BOT_USERNAME as bu, IMG_BOT, SUPPORT_CHAT
 
-PHOTO = "https://telegra.ph/file/f268a324304deaa0f0180.jpg"
+
+PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm {dispatcher.bot.first_name}.** \n\n"
-  TEXT += "⚡️ **I'm Working Properly** \n\n"
-  TEXT += f"⚡️ **My Master : [master](t.me/{uname})** \n\n"
-  TEXT += f"⚡️ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚡️ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚡️ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
+  TEXT += "⚪ **I'm Working Properly** \n\n"
+  TEXT += f"⚪ **My Master : [Bion](https://t.me/onlybionn)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", f"https://t.me/{bu}?start=help"), Button.url("Support", f"https://t.me/{SUPPORT_CHAT}")]]
+  BUTTON = [[Button.url("Help", "https://t.me/zenitsuclonev1bot?start=help"), Button.url("Support", "https://t.me/zennih")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
