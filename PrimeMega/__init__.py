6f09fa599377d3728dfe375e44fab967d03d185b
BionXP <112658830+BionXP@users.noreply.github.com>
2022-11-18 10:38:44 +0800

    Update __init__.py

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -229,7 +229,7 @@ else:
         sw = None
         LOGGER.warning("Can't connect to SpamWatch!")
 
-from EmikoRobot.modules.sql import SESSION
+from PrimeMega.modules.sql import SESSION
 
 defaults = tg.Defaults(run_async=True)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
