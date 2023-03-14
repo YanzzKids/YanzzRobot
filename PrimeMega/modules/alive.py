eef583c68a95115b74f9669cc6b0057e6728598f
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-14 08:23:22 +0800

    Update alive.py

--- PrimeMega/modules/alive.py
+++ PrimeMega/modules/alive.py
@@ -20,5 +20,5 @@ async def awake(event):
   TEXT += f"🔰 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"💥 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/zenitsuclonev1bot?start=help"), Button.url("Support", "https://t.me/zennih")]]
+  BUTTON = [[Button.url("Help", "https://t.me/YanzzRobot?start=help"), Button.url("Support", "https://t.me/YanzzSupportt")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
